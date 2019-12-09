c05564e222e0127548392db3861903decf40b679
Javier Juarez <javierjuarez.jj28@gmail.com>
2019-12-09 15:25:26 -0600

    PrimeraVersion
    Primera version del proyecto de tecnologias "Juego de loteria"

---
+++ CalculatorService/Host/Program.cs
@@ -0,0 +1,24 @@
+﻿using CalculatorService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Host
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (ServiceHost host = new ServiceHost(typeof(CalculatorService.CalculatorService)))
+            {
+                host.Open();
+                Console.WriteLine("El servidor está arriba");
+                Console.ReadLine();
+            }
+        }
+
+    }
+}
